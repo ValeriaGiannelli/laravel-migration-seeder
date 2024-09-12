74837ff91d92a797decb00f9c218cf04bdf28611
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-12 14:10:22 +0200

    fix: change variable into static tag

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,10 @@
 
 @section('content')
 <div class="container my-5">
-    <h1>{{ $title }}</h1>
+    <h1>Titolo</h1>
     <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
     <p>
-        {{ $text }}
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis eius ab, iure, expedita velit beatae, accusantium officiis omnis magnam fuga quod quae nesciunt culpa voluptates sapiente. Adipisci repellendus perferendis nisi.
     </p>
 </div>
 
