145f45f03c21516acaf484419d2aa88b504ed515
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-12 15:31:56 +0200

    feat: insert if condition to display only the future trains, anche change the display of time and date Base exercise completed

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -14,21 +14,23 @@
             <th scope="col">Treno numero</th>
             <th scope="col">In patenza da</th>
             <th scope="col">Arriva a</th>
+            <th scope="col">Giorno</th>
             <th scope="col">Delle ore</th>
             <th scope="col">Ritardo</th>
-            <th scope="col">Cancellato</th>
           </tr>
         </thead>
         <tbody>
             @foreach ( $trains as $train )
                 <tr>
-                    <td>{{$train -> id}}</td>
-                    <td>{{$train -> code}}</td>
-                    <td>{{$train -> departure_station}}</td>
-                    <td>{{$train -> arrival_station}}</td>
-                    <td>{{$train -> departure_time}}</td>
-                    <td>{{$train -> on_time}}</td>
-                    <td>{{$train -> cancelled}}</td>
+                    @if($train->date >= Carbon\Carbon::now()->format('Y-m-d'))
+                        <td>{{$train -> id}}</td>
+                        <td>{{$train -> code}}</td>
+                        <td>{{$train -> departure_station}}</td>
+                        <td>{{$train -> arrival_station}}</td>
+                        <td>{{Carbon\Carbon::parse($train -> date)->format('d-m-Y')}}</td>
+                        <td>{{Carbon\Carbon::parse($train->departure_time)->format('H:i')}}</td>
+                        <td>{{$train -> on_time}}</td>
+                    @endif
                 </tr>
             @endforeach
 
